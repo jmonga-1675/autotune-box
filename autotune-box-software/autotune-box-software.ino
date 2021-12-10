8d238983b17afaff7b798d35c0372f11cae56e13
Jay Monga <jay.monga@gmail.com>
2021-12-09 18:45:18 -0800

    yeet

--- autotune-box-software/autotune-box-software.ino
+++ autotune-box-software/autotune-box-software.ino
@@ -1,4 +1,6 @@
 #include "driver/i2s.h"
+#include "arduinoFFT.h"
+
 // Pin definitions
 // Display
 const uint8_t DIG1_A = GPIO_NUM_19;
@@ -15,35 +17,39 @@ const uint8_t RECORD = GPIO_NUM_26;
 
 // MIC
 const uint8_t AUDIO_IN = GPIO_NUM_26;
-const uint8_t SCK = GPIO_NUM_27;
-const uint8_t WS = GPIO_NUM_14;
+const uint8_t AUDIO_SCK = GPIO_NUM_27;
+const uint8_t AUDIO_WS = GPIO_NUM_14;
 
 // Mic out
 const uint8_t AUDIO_OUT = GPIO_NUM_25;
 
 // I2S Port
-const i2s_port_t I2S_PORT = I2S_NUM_0;
+const i2s_port_t I2S_PORT = I2S_NUM_1;
+
+// FFT Params
+const uint16_t SAMPLE_FREQUENCY = 32000;
 
 void setup() {
   Serial.begin(115200);
   esp_err_t err;
 
-  // The I2S config as per the example
-  const i2s_config_t i2s_config = {
-      .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX), // Receive, not transfer
-      .sample_rate = 16000,                         // 16KHz
-      .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT, // could only get it to work with 32bits
-      .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT, // use right channel
-      .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
-      .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,     // Interrupt level 1
-      .dma_buf_count = 4,                           // number of buffers
-      .dma_buf_len = 8                              // 8 samples per buffer (minimum)
-  };
+  i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+    .sample_rate = 20000,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S),
+    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+    .dma_buf_count = 4,
+    .dma_buf_len = 1024,
+    .use_apll = false,
+    .tx_desc_auto_clear = false,
+    .fixed_mclk = 0};
 
   // The pin config as per the setup
   const i2s_pin_config_t pin_config = {
-      .bck_io_num = SCK,   // Serial Clock (SCK)
-      .ws_io_num = WS,    // Word Select (WS)
+      .bck_io_num = AUDIO_SCK,   // Serial Clock (SCK)
+      .ws_io_num = AUDIO_WS,    // Word Select (WS)
       .data_out_num = I2S_PIN_NO_CHANGE, // not used (only for speakers)
       .data_in_num = AUDIO_IN   // Serial Data (SD)
   };
@@ -65,5 +71,15 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  bool record_state = digitalRead(RECORD);
+  if (record_state){
+    // Record many data
+
+    // FFT data in set windows
+
+    // Set all data in window to frequency of window
+
+    // Send data to speaker
+  }
 
 }
